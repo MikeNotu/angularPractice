4cc886ee4f51a377a497892e35bbd7cc218185b4
MikeNotu <michael.bateman@globant.com>
2022-11-10 16:40:10 -0600

    app module ts update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Attendant } from './Attendant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularPractice';
+  // title = 'angularPractice';
+  attendants: Attendant[] = [];
+  newAttendant: Attendant = {
+    name: '',
+    arrivedAt: '',
+  };
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      alert('Formulario inválido');
+      return;
+    }
+    this.attendants.push(this.newAttendant);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.newAttendant = {
+      name: '',
+      arrivedAt: '',
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AttendantItemComponent } from './attendant-item/attendant-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AttendantItemComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AttendantItemComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
